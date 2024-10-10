a125442a052216c22802141884bbe94c4e5bb523
MinjooShin <als000313@naver.com>
2024-10-11 01:44:43 +0900

    refactor: client 스크립트 수정

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-if [ ! -d ~/bin ]; then
+cd ~/Baby-Crying-Monitoring
+
+if [ ! -d ~/Baby-Crying-Monitoring/bin ]; then
     mkdir ~/bin
 fi
 
-if [ -f ~/bin/Client ]; then
-    rm -f ~/bin/Client
+if [ -f ~/Baby-Crying-Monitoring/bin/client ] || [ -f ~/Baby-Crying-Monitoring/bin/dht22_reader ]; then
+    make clean
 fi
 
-g++ -o ~/bin/Client Client.cc `pkg-config —cflags —libs gstreamer-1.0 gstreamer-rtsp-server-1.0`
+make
 
-~/bin/Client
+# 빌드 성공하면 실행
+if [ -f ~/Baby-Crying-Monitoring/bin/client ]; then
+    ~/Baby-Crying-Monitoring/bin/client
+fi
+
+if [ -f ~/Baby-Crying-Monitoring/bin/dht22_reader ]; then
+    ~/Baby-Crying-Monitoring/bin/dht22_reader
+fi
